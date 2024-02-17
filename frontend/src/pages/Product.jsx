dc0ad4828facc35ad809dbf6a7c791ce5a74c98f
Riocodex <onwukachibike@gmail.com>
2024-02-17 10:44:27 +0100

    created the product display page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../components/Breadcrumbs/Breadcrum'
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../components/RelatedProducts/RelatedProducts'
 
 const Product = () => {
     const {all_product} = useContext(ShopContext)
@@ -13,6 +16,9 @@ const Product = () => {
   return (
     <div>
       <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
+      <DescriptionBox/>
+      <RelatedProducts/>
     </div>
   )
 }
