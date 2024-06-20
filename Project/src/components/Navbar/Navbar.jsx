c18a9f82db08dd888e82f672d31981282f2552ef
Riocodex <onwukachibike@gmail.com>
2024-06-20 18:15:39 +0200

    updated bugs and errors

--- Project/src/components/Navbar/Navbar.jsx
+++ Project/src/components/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ export const Navbar = () => {
         <div className="nav-login-cart">
             <Link to='/login'><button> Login </button></Link>
             <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-           <div className="nav-cart-count">{getTotalCartItems()}</div>
+           <div className="nav-cart-count">{getTtalCartItems()}</div>
         </div>
     </div>
   )
