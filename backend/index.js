af758259ee9329128886cae1e522c64dba0b182e
Riocodex <onwukachibike@gmail.com>
2024-06-22 01:23:45 +0200

    updated imagepost api

--- backend/index.js
+++ backend/index.js
@@ -32,12 +32,12 @@ const storage = multer.diskStorage({
 const upload = multer({storage:storage})
 
 //creating upload endpoint for images
-
+app.use('/images',express.static('upload/images'))
 
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:``
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
     })
 })
 
