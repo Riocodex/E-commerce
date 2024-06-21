10df33854c143148bd6a2ce359815ab91273405c
Riocodex <onwukachibike@gmail.com>
2024-06-22 00:45:37 +0200

    updated and established mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,11 @@ const jwt = require("jsonwebtoken")
 const multer = require("multer")
 const path =  require("path")
 const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+
+//database connection wtih mongodb
+mongoose.connect("mongodb+srv://onwukachibike:NC3TpUnyNjez0oNg@cluster0.t8msajv.mongodb.net/e-commerce")
+ 
+//
---
+++ readme.md
@@ -0,0 +1,3 @@
+NC3TpUnyNjez0oNg
+
+mongodb+srv://onwukachibike:NC3TpUnyNjez0oNg@cluster0.t8msajv.mongodb.net/
