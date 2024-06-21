200674b0186fb22258dc68d7b9f3c6663ae76ce6
Riocodex <onwukachibike@gmail.com>
2024-06-22 01:12:25 +0200

    updated disk storage

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,10 @@ app.get("/",(req, res)=>{
 //image storage engine
 
 const storage = multer.diskStorage({
-    destination:"./upload"
+    destination:"./upload/images",
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
 })
 
 
