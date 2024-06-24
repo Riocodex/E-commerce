ad87106a7493b158a5022d0c68d8c43c855bfcea
Riocodex <onwukachibike@gmail.com>
2024-06-24 20:52:05 +0200

    updated

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Sidebar.css"
 import { Link } from 'react-router-dom'
 import add_product_icon from '../../assets/Product_Cart.svg'
-import list_product_icon from '../../assets/Product_list_icon.svg
+import list_product_icon from '../../assets/Product_list_icon.svg'
 
 const Sidebar = () => {
   return (
