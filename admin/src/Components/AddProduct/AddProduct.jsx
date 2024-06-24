6a6a103c300ddbd25c2146d37d07ed342db17859
Riocodex <onwukachibike@gmail.com>
2024-06-24 21:18:18 +0200

    updated

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -21,6 +21,28 @@ const AddProduct = () => {
         setProductDetails({...productDetails,[e.target.name]:e.target.value,})
     }
 
+    const Add_Product = async()=>{
+        console.log(productDetails)
+        let responseData;
+        let product = productDetails;
+
+        let formData = new FormData();
+        formData.append('product',image);
+
+        await fetch('http://localhost:4000/upload',{
+            method:'POST',
+            headers:{
+                Accept:'application/json',
+            },
+            body:formData,
+        }).then((resp)=>resp.json()).then((data)=>{responseData=data})
+
+        if(responseData.success){
+            product.image = responseData.image_url;
+            console.log(product)
+        }
+    }
+
   return (
     <div className='add-product'>
         <div className="addproduct-itemfield">
@@ -53,7 +75,7 @@ const AddProduct = () => {
             </label>
             <input onChange={imageHandler} type="file" name='image' id='file-input' hidden />
         </div>
-        <button className='addproduct-btn'>ADD</button>
+        <button onClick={()=>{Add_Product()}} className='addproduct-btn'>ADD</button>
        
     </div>
   )
