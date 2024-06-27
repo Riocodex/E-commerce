a9e44239abd9b14257b154330deefcd446e5d2b3
Riocodex <onwukachibike@gmail.com>
2024-06-27 13:10:47 +0200

    adding product to tdatabase from admin works succesfulluy

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ListProduct.css'
 
 const ListProduct = () => {
+
+    const [allproducts, setAllProducts] = useState([])
+    
+
   return (
     <div className='list-product'>
-
+            <h1>All products list</h1>
+            <div className="listproduct-format-main">
+                <p>Products</p>
+                <p>Title</p>
+                <p>Old price</p>
+                <p>New price</p>
+                <p>Category</p>
+                <p>Remove</p>
+            </div>
+            <div className="listproduct-allproducts">
+                <hr />
+            </div>
     </div>
   )
 }
