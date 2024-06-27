30cc96b79256acaba517b6c507259eee50213ccb
Riocodex <onwukachibike@gmail.com>
2024-06-27 13:10:56 +0200

    updated

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -4,14 +4,14 @@ import './ListProduct.css'
 const ListProduct = () => {
 
     const [allproducts, setAllProducts] = useState([])
-    
+
 
   return (
     <div className='list-product'>
             <h1>All products list</h1>
             <div className="listproduct-format-main">
                 <p>Products</p>
-                <p>Title</p>
+                <p>Tite</p>
                 <p>Old price</p>
                 <p>New price</p>
                 <p>Category</p>
