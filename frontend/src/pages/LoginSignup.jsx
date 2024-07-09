82cc0d87e5058b24dfb332d506347b2baa042960
Riocodex <onwukachibike@gmail.com>
2024-07-09 08:42:18 +0200

    updated

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -15,11 +15,27 @@ const LoginSignup = () => {
   }
 
   const login = async ()=>{
-      console.log("login")
+      console.log("login",formData)
   }
 
   const signup = async ()=>{
-    console.log("signup")
+    console.log("signup",formData)
+    let responseData;
+    await fetch('http://localhost:4000/signup',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    }).then((response)=>response.json()).then((data)=>responseData=data)
+
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token);
+      window.location.replace("/")
+    }else{
+      alert(responseData.errors)
+    }
   }
 
   return (
@@ -28,8 +44,8 @@ const LoginSignup = () => {
           <h1>{state}</h1>
           <div className="loginsignup-fields">
             {state ==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name'/>:<></>}
-            <input name='email' value={formData.email} type="email" placeholder='Email Address'/>
-            <input type="password" placeholder='Password'/>
+            <input name='email' value={formData.email} onChange={changeHandler}  type="email" placeholder='Email Address'/>
+            <input name='password' value={formData.password} onChange={changeHandler}  type="password" placeholder='Password'/>
           </div>
           <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
           {state === "Sign Up"?<p className="loginsignuplogin">
