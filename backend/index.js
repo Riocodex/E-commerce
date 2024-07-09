f7c726bdafc346aa201af6e980afcfb3a8b3c594
Riocodex <onwukachibike@gmail.com>
2024-07-09 07:01:48 +0200

    creating jwt

--- backend/index.js
+++ backend/index.js
@@ -155,8 +155,31 @@ app.post('/signup',async(req,res)=>{
 
     let check = await Users.findOne({email:req.body.email})
     if(check){
-        return
+        return res.status(400).json({success:false, errors:"existing user found with same email id"})
     }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        
+        cart[i] = 0;
+
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+        
+    }
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
 })
 
 
