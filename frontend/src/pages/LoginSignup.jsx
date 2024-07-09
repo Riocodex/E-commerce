e86b600e1921a6571f79dfc72ce68b0d8760ed60
Riocodex <onwukachibike@gmail.com>
2024-07-09 08:08:45 +0200

    creating endpoint for user login

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
+
+  const [state, setState ] = useState("Login")
+
   return (
     <div className='loginsignup'>
         <div className="loginsignup-container">
-          <h1>Sign Up</h1>
+          <h1>{state}</h1>
           <div className="loginsignup-fields">
-            <input type="text" placeholder='Your Name'/>
+            {state ==="Sign up"?<input type="text" placeholder='Your Name'/>:<></>}
             <input type="email" placeholder='Email Address'/>
             <input type="password" placeholder='Password'/>
           </div>
           <button>Continue</button>
-          <p className="loginsignuplogin">
+          {state === "Sign Up"?<p className="loginsignuplogin">
             Already have an account? <span>Login</span>
-          </p>
+          </p>: <p className="loginsignuplogin">
+            Create an account? <span>Click here</span>
+          </p>}
+          
+         
           <div className="loginsignup-agree">
             <input type="checkbox" name='' id='' />
             <p>By continuing, I agree to the terms of use & privacy policy</p>
