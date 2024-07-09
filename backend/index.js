c5debaeb68649027232a1272ae69ba3df1c7b9d9
Riocodex <onwukachibike@gmail.com>
2024-07-09 23:28:19 +0200

    adding middle ware to fetch user

--- backend/index.js
+++ backend/index.js
@@ -218,6 +218,22 @@ app.post('/addtocart',async(req,res)=>{
 })
 
 //creating middleware to fetch user
+    const fetchUser = async(req,res,next)=>{
+        const token = req.header('auth-token')
+        if(!token){
+            res.status(401).send({erros:"please authenticate using valid token"})
+
+        }
+        else{
+            try{
+                const data = jwt.verify(token,'secret_ecom')
+                req.user = data.user;
+                next()
+            }catch(error){
+                res.status(401).send({errors:"please authenticate using a valid id"})
+            }
+        }
+    }
 
 
 app.listen(port,(error)=>{
