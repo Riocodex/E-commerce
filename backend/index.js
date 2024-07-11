091598d8143dd4d2718b7d3210ff3c260608ae8d
Riocodex <onwukachibike@gmail.com>
2024-07-11 09:39:41 +0200

    updated

--- backend/index.js
+++ backend/index.js
@@ -236,6 +236,18 @@ app.get('/newcollections',async(req,res)=>{
 app.post('/addtocart',fetchUser,async(req,res)=>{
     let userData = await Users.findOne({_id:req.user.id})
     userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+    res.send("Added")
+})
+
+//creating endpoint to remove product from cartdata
+app.post('/removefromcart', fetchUser,async()=>{
+    console.log("removed",req.body.itemId)
+    let userData = await Users.findOne({_id:req.user.id});
+    if(userData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
+    res.send("Removed")
 })
 
 
